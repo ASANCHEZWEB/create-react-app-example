e21b44b3229239b0f1e69c6a6f091bf9bb44babb
ASANCHEZWEB <desarrollosalbertosanchez@gmail.com>
2020-05-02 18:36:29 +0200

    Convertir valores textarea y select como input mediante controlador

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import DynamicMoviesList from "./components/dynamicListsDemo/DynamicMoviesList";
+import TextareaAndSelectTagDemo from './components/dynamicListsDemo/TextareaAndSelectTagDemo';
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <DynamicMoviesList />
+        <TextareaAndSelectTagDemo />
       </div>
     );
   }
